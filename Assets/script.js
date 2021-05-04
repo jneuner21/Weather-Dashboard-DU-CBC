f75d4de0da6179388a480b89ceb201dc5e8e3bac
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-05-04 17:46:41 -0600

    updated javascript to display 5 day forcast instead of it being coded into html

--- Assets/script.js
+++ Assets/script.js
@@ -15,8 +15,30 @@ $(document).ready(function(){
     var temp = Math.floor(data.main.temp);
     var wind = data.wind.speed;
     var humidity = data.main.humidity;
-  
-    $("image").attr("src", icon);
+    var lon = data.coord.lon
+    var lat = data.coord.lat
+
+    $.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=452ac1fbf4b642ff68e5b5bf6ad128c6", function(data){
+
+    data.daily.forEach(function(day,index) {
+      console.log(day)
+
+      if (index <= 5 && index >=1){
+        $(".cardSpacer").append(`<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Future Weather</h5>
+          <p class="card-text">${day.temp.day}</p>
+          <p class="card-text">${day.wind_speed}</p>
+          <p class="card-text">${day.humidity}</p>
+        </div>
+      </div>`)
+      }
+      
+    });
+      
+    });
+
+    $("image").attr("src", + icon);
     $(".temp").text("Tempeture: " + temp);
     $(".wind").text("Wind: " + wind);
     $(".humidity").text("Humidity: " + humidity);
@@ -26,6 +48,7 @@ $(document).ready(function(){
   });
   
   $("#buttonList").click(function(e){
+
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + e.target.innerText + "&units=imperial&APPID=452ac1fbf4b642ff68e5b5bf6ad128c6", function(data){
       var icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
       var temp = Math.floor(data.main.temp);
