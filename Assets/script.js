a74b2c1685c8a3fec4d8e510af5f178c28357cd2
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-05-03 21:56:38 -0600

    updated javasript to react to click listens and display previous inputs, allow user to go back, fixed hyml to include all 5 day forcast cards, modified javascript to show icones with weather when clicked

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,45 @@
 $(document).ready(function(){
+ 
+  var cityValue = $("input").val();
+  $(".cityName").text(cityValue);
 
   $("#searhcBtn").click(function(){
     $(".fiveDayForcast").show();
-    $(".previousCity").show();
     $(".weatherContainer").show();
     var cityValue = $("input").val();
     $(".cityName").text(cityValue);
+    $("#buttonList").append(`<button type="button" id="preCity" class="list-group-item list-group-item-action d-flex justify-content-center">`+ cityValue +`</button>`)
 
-    console.log(cityValue)
-    
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + cityValue + "&units=imperial&APPID=452ac1fbf4b642ff68e5b5bf6ad128c6", function(data){
-  
-    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
     var temp = Math.floor(data.main.temp);
     var wind = data.wind.speed;
     var humidity = data.main.humidity;
   
-    $(".icon").attr("src", icon);
-    $(".temp").append(temp);
-    $(".wind").append(wind);
-    $(".humidity").append(humidity);
-  
-    });
-    
-    //var index = ("input");
-
-    //var searchHist = $(index,);// this line needs to give a value to whatever input is entered
-
-   // parseInt($("input").attr());
-
-    //localStorage.setItem(searchHist, cityValue);
-    
+    $("image").attr("src", icon);
+    $(".temp").text("Tempeture: " + temp);
+    $(".wind").text("Wind: " + wind);
+    $(".humidity").text("Humidity: " + humidity);
   });
 
 
+  });
+  
+  $("#buttonList").click(function(e){
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + e.target.innerText + "&units=imperial&APPID=452ac1fbf4b642ff68e5b5bf6ad128c6", function(data){
+      var icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+      var temp = Math.floor(data.main.temp);
+      var wind = data.wind.speed;
+      var humidity = data.main.humidity;
+      
+      $(".cityName").text(e.target.innerText);
+      $(".icon").attr("src", icon);
+      $(".temp").text("Tempeture: " + temp);
+      $(".wind").text("Wind: " + wind);
+      $(".humidity").text("Humidity: " + humidity);
+    });
+
+  });
 
 
 });
